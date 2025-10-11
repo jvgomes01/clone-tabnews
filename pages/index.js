2526189e738c68ef1426326d1dbd8f1717498f28
Joao Victor Gomes <61714929+jvgomes01@users.noreply.github.com>
2025-10-11 12:18:56 +0000

    alteracao na frase

--- pages/index.js
+++ pages/index.js
@@ -1,6 +1,9 @@
 function Home(){
-    var msg = "Sempre por ti e para ti. Amo-te infinitamente, Isadora<3!!"
-    return <h1>{msg}</h1>
+    var msg = "Sempre por ti e para ti. Amo-te infinitamente, Isadora<3!!!"
+    return (
+    <h1>{msg}</h1>
+        
+    )
 }
 
 export default Home;
