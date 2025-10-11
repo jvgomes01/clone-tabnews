d72adcc5db1f04ec72ec5edde893f1bf34ee8d28
Joao Victor Gomes <61714929+jvgomes01@users.noreply.github.com>
2025-10-11 13:09:40 +0000

    novas ideias

--- pages/index.js
+++ pages/index.js
@@ -1,9 +1,30 @@
-function Home(){
-    var msg = "Sempre por ti e para ti. Amo-te infinitamente, Isadora<3!!!"
-    return (
-    <h1>{msg}</h1>
-        
-    )
+export default function Home() {
+  return (
+    <div
+      style={{
+        backgroundImage:
+          "linear-gradient(rgba(0,0,0,0.6), rgba(0,0,0,0.6)), url('https://wallpapercave.com/wp/iy77JdQ.gif')",
+        backgroundSize: "cover",
+        backgroundPosition: "center",
+        height: "100vh",
+        display: "flex",
+        flexDirection: "column",
+        alignItems: "center",
+        justifyContent: "center",
+        color: "white",
+        textAlign: "center",
+      }}
+    >
+      <h1>Oi meu amor, Te amo muito 💖</h1>
+      <img
+        src="https://img2.lovecell.com.br/c6a3128fa49362134e9770e615f52e72762e22583dc31d8369981386a65f81f8.webp"
+        alt="Flor"
+        width={300}
+        height={300}
+        style={{ borderRadius: "10px", marginBottom: "20px" }}
+      />
+      <h2>Uma flor pra outra flor 🌹</h2>
+      
+    </div>
+  );
 }
-
-export default Home;
